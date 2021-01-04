a8b6934390a49c32504d78854e0d6161d43b4425
peace920902 <peace9209021@gmail.com>
2021-01-04 23:24:43 +0800

    fix tab problem

--- Lazcat.Blog.Web/wwwroot/javascript/jsinerop/jsinterop.js
+++ Lazcat.Blog.Web/wwwroot/javascript/jsinerop/jsinterop.js
@@ -1,8 +1,17 @@
 ﻿window.jsinteropFunc = {
     preventTabEvent: function() {
-        document.getElementById("PreventTabTextArea").addEventListener("keydown",function (event) {
+        document.getElementById("PreventTabTextArea").addEventListener("keydown", function (event) {
                 if (event.keyCode=== 9) {
                     event.preventDefault();
+                    var start = this.selectionStart;
+                    var end = this.selectionEnd;
+
+                    // set textarea value to: text before caret + tab + text after caret
+                    this.value = this.value.substring(0, start) +
+                        "\t" + this.value.substring(end);
+
+                    // put caret at right position again
+                    this.selectionStart = this.selectionEnd = start + 1;
                 }
             }
         );
