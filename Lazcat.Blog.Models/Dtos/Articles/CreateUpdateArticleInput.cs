d6e5a5e70ac21ccdc7f77442ef94bd3843557922
peace920902 <peace9209021@gmail.com>
2021-01-17 22:18:22 +0800

    add some css

--- Lazcat.Blog.Models/Dtos/Articles/CreateUpdateArticleInput.cs
+++ Lazcat.Blog.Models/Dtos/Articles/CreateUpdateArticleInput.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Lazcat.Blog.Models.Dtos.Articles
 {
@@ -8,6 +7,8 @@ namespace Lazcat.Blog.Models.Dtos.Articles
         public int Id { get; set; }
         [Required, MaxLength(50, ErrorMessage = "Title length should less than 50")]
         public string Title { get; set; }
+        [MaxLength(100, ErrorMessage = "Description length should less than 100")]
+        public string Description { get; set; }
         public string Content { get; set; }
         [Required, Range(1, int.MaxValue) ]
         public int CategoryId { get; set; }
